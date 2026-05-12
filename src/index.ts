71247b50f70316496e4002e35143e56b9f7de08d
Dhruv Choudhary <dhruvchoudhary38@gmail.com>
2026-05-12 20:38:04 +0000

    last push

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,9 @@ import express from 'express';
 const app = express();
 
 app.get("/",(req,res)=>{
-res.json({ message: "hello"});
+res.json({ message: "hello",
+    testing:"passed"
+});
 });
 
 app.listen(3000,()=>{
